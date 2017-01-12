6f1f745850de7dc3c80f6ef2968ea57238e3f908
Bell Codo <bellcodo@gmail.com>
2017-01-12 08:42:38 -0800

    Create Character.py
    copy of the character class

---
+++ src/Character.py
@@ -0,0 +1,121 @@
+class Character:
+    key_map = {
+        "left": 37,
+        "up"  : 38,
+        "right":39,
+        "down": 40
+    }
+
+    move_dist = 5
+    vel = [move_dist, 0]
+     
+    def __init__ (self):
+        self.circle_shape = Circle()
+        self.shape_attributes = ShapeAttributes()
+        self.body = Body()
+         
+    def bumps_into_left_wall(self):
+        return self.circle_shape.center_point[0] <= self.circle_shape.radius
+            
+    def morph_into_red_dot(self):
+        self.shape_attributes.fill_color = "Red"
+        self.circle_shape.radius = 50
+        
+    def morph_into_black_dot(self):
+        self.shape_attributes.fill_color = "Black"
+        self.circle_shape.radius = 9
+
+    def morph_into_pink_dot(self):
+        self.shape_attributes.fill_color = "Pink"
+        self.circle_shape.radius = 30
+        
+    def morph_into_purple_dot(self):
+        self.shape_attributes.fill_color = "Purple"
+        self.circle_shape.radius = 60
+        
+    def bumps_into_right_wall(self):
+        return self.circle_shape.center_point[0] >= WINDOW_WIDTH-self.circle_shape.radius
+    
+    def bumps_into_ceiling(self):
+        return self.circle_shape.center_point[1] <= self.circle_shape.radius
+    
+    def bumps_into_floor(self):
+        return self.circle_shape.center_point[1] >= WINDOW_HEIGHT-self.circle_shape.radius 
+    
+    def save_me(self):
+        segment = self.circle_shape.center_point
+        self.body.append(segment)
+        
+        
+    #draw a circle on canvas
+    #(center_point, radius, line_width, line_color, fill_color = color
+    def draw_me (self, canvas):
+        
+        #=================add=======================
+        #self.circle_shape.center_point[0] += Character.vel[0]
+        # can't change value above. but can point to different reference
+        self.circle_shape.center_point = (
+            self.circle_shape.center_point[0] +  Character.vel[0],
+            self.circle_shape.center_point[1] + Character.vel[1]
+        )
+        
+        if self.bumps_into_left_wall():
+            self.morph_into_red_dot()
+        
+        if self.bumps_into_right_wall():
+            self.morph_into_black_dot()
+            
+        if self.bumps_into_ceiling():
+            self.morph_into_pink_dot()
+            
+        if self.bumps_into_floor():
+            self.morph_into_purple_dot()
+        
+        #self.circle_shape.center_point[1] += vel[1]
+           
+        canvas.draw_circle (
+            self.circle_shape.center_point,
+            self.circle_shape.radius,
+            self.shape_attributes.line_width,
+            self.shape_attributes.line_color,
+            self.shape_attributes.fill_color
+        )
+        
+        for segment in self.body.list_segments():
+            canvas.draw_circle(
+                segment,
+                self.circle_shape.radius /2,
+                self.shape_attributes.line_width,
+                self.shape_attributes.fill_color,
+                self.shape_attributes.fill_color    
+            )
+    
+    # get input key press and move the circle
+    def move (self, key):
+        
+        #check if key is in key_map array. 
+        #Character: this class name
+        if key in Character.key_map.values():
+            if key == Character.key_map["right"]:
+                print "move right"
+                
+                #self.circle_shape.update_x(Character.move_dist)
+                Character.vel = [Character.move_dist, 0]
+                
+            if key == Character.key_map["left"]:
+                print "move left"
+                
+                #self.circle_shape.update_x(-Character.move_dist)    
+                Character.vel = [-Character.move_dist, 0]
+            
+            if key == Character.key_map["up"]:
+                print "move up"
+                
+                #self.circle_shape.update_y(-Character.move_dist)
+                Character.vel = [0, -Character.move_dist]
+        if key == Character.key_map["down"]:
+                print "move down"
+                
+                #self.circle_shape.update_y(Character.move_dist)
+                Character.vel = [0, Character.move_dist]
+    
