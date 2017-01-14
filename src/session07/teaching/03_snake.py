7941209a49ccc2a42137f1410df09e335e18374d
Bell Codo <bellcodo@gmail.com>
2017-01-14 15:08:04 -0800

    Create 03_snake.py

---
+++ src/session07/teaching/03_snake.py
@@ -0,0 +1,17 @@
+class Snake:
+    def __init__(self, square):
+        self.head = square
+        self.snake_map = {}
+        self.add(square)
+    
+    def add(self, square):
+        point = square.point
+        pos_key = "%s-%s" % (point[0], point[1])
+        self.snake_map[pos_key] = square
+        self.head = square
+        
+    def move_left(self):
+        x = self.head.point[0] - 1
+        y = self.head.point[1]
+        new_square = Square((x,y), self.head.size)
+        self.add(new_square)
