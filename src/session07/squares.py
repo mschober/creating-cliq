87e6bcb32d339c02eaf62cb733658c936e387672
Bell Codo <bellcodo@gmail.com>
2017-01-14 14:44:25 -0800

    Update squares.py

--- src/session07/squares.py
+++ src/session07/squares.py
@@ -2,44 +2,44 @@ import simplegui
 
 # abstract the points based on the size
 
-size = 50
-grid_spot = 3,2
-
-def square_coords_from_grid(size, grid_spot):
-    x = grid_spot[0]
-    y = grid_spot[1]
-    points = [(x,y), (x+1,y), (x+1,y+1), (x,y+1)]
-    scaled_points = map(lambda point:
-                        (point[0]*size,point[1]*size), 
-                    points)
-    print scaled_points
-    return scaled_points
-
-points = square_coords_from_grid(size, grid_spot)
-
-def draw(canvas):
-    canvas.draw_polygon(
-        points, 3, "Blue", "Green"    
-    )
-
 keymap = {
     "left": 37,
     "right": 39,
     "down": 40,
     "up": 38
-}    
+}
+        
+class Square(object):
+    def __init__(self, point, size):
+        self.size = size
+        self.point = point
+        self.points = self.square_coords_from_grid(size, point)
     
-def move(key):
-    if keymap["left"] == key:
-        print "left"
-    elif keymap["right"] == key:
-        print "right"
-    elif keymap["up"] == key:
-        print "up"
-    elif keymap["down"] == key:
-        print "down"
-    else :
-        print "unused"
+    def square_coords_from_grid(self, size, grid_spot):
+        x = grid_spot[0]
+        y = grid_spot[1]
+        points = [(x,y), (x+1,y), (x+1,y+1), (x,y+1)]
+        scaled_points = map(lambda point:
+                            (point[0]*size,point[1]*size), 
+                        points)
+        print scaled_points
+        return scaled_points
+    
+    def draw(self, canvas):
+        canvas.draw_polygon(
+        self.points, 3, "Blue", "Green"  
+    )
+        
+home_square = Square((0, 0), 50)
+
+the_snake = {
+    str(home_square.point[0]) + "-" + str(home_square.point[1]): home_square    
+}
+
+def draw(canvas):
+    for square in the_snake.values():
+        square.draw(canvas)
+
     
 frame = simplegui.create_frame("Home", 500, 500)
 frame.set_draw_handler(draw)
