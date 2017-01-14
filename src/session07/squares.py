720e73fd32e8876e082e1f70594df4cd3245e80f
Bell Codo <bellcodo@gmail.com>
2017-01-14 14:58:13 -0800

    Update squares.py

--- src/session07/squares.py
+++ src/session07/squares.py
@@ -29,15 +29,44 @@ class Square(object):
         canvas.draw_polygon(
         self.points, 3, "Blue", "Green"  
     )
-        
-home_square = Square((0, 0), 50)
 
-the_snake = {
-    str(home_square.point[0]) + "-" + str(home_square.point[1]): home_square    
-}
+class Snake:
+    def __init__(self, square):
+        self.head = square
+        self.snake_map = {}
+        self.add(square)
+    
+    def add(self, square):
+        point = square.point
+        pos_key = "%s-%s" % (point[0], point[1])
+        self.snake_map[pos_key] = square
+        self.head = square
+        
+    def move_left(self):
+        x = self.head.point[0] - 1
+        y = self.head.point[1]
+        new_square = Square((x,y), self.head.size)
+        self.add(new_square)
+      
+home_square = Square((5, 5), 50)
+the_snake = Snake(home_square)
+  
+def move(key):
+    x = home_square.point[0]
+    y = home_square.point[1]
+    if keymap["left"] == key:
+        the_snake.move_left()
+    elif keymap["right"] == key:
+        print "right"
+    elif keymap["up"] == key:
+        print "up"
+    elif keymap["down"] == key:
+        print "down"
+    else :
+        print "unused"
 
 def draw(canvas):
-    for square in the_snake.values():
+    for square in the_snake.snake_map.values():
         square.draw(canvas)
 
     
