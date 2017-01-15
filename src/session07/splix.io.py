9e428cd6b9edc372c884ebc3286e0cc80cf53747
Bell Codo <bellcodo@gmail.com>
2017-01-14 16:54:10 -0800

    Update splix.io.py

--- src/session07/splix.io.py
+++ src/session07/splix.io.py
@@ -1,6 +1,6 @@
 import simplegui
 
-SIZE = 50
+SIZE = 25
 SNAKE_COLOR = ("Gold", "Purple")
 WINDOW = (500,500)
 LEFT = "LEFT"
@@ -15,21 +15,21 @@ KEYMAP = {
     UP: 38
 }
 
+def square_coords_from_grid(size, grid_spot):
+    x = grid_spot[0]
+    y = grid_spot[1]
+    points = [(x,y), (x+1,y), (x+1,y+1), (x,y+1)]
+    scaled_points = map(lambda point:
+                        (point[0]*size,point[1]*size), 
+                        points)
+    print scaled_points
+    return scaled_points
+
 class Square(object):
     def __init__(self, point, size):
         self.size = size
         self.point = point
-        self.points = self.square_coords_from_grid(size, point)
-    
-    def square_coords_from_grid(self, size, grid_spot):
-        x = grid_spot[0]
-        y = grid_spot[1]
-        points = [(x,y), (x+1,y), (x+1,y+1), (x,y+1)]
-        scaled_points = map(lambda point:
-                            (point[0]*size,point[1]*size), 
-                        points)
-        print scaled_points
-        return scaled_points
+        self.points = square_coords_from_grid(size, point)
     
     def draw(self, canvas):
         canvas.draw_polygon(
@@ -53,6 +53,65 @@ class Snake:
         y = self.head.point[1]
         new_square = Square((x,y), self.head.size)
         self.add(new_square)
+        
+    def move_right(self):
+        x = self.head.point[0] + 1
+        y = self.head.point[1]
+        new_square = Square((x,y), self.head.size)
+        self.add(new_square)
+    
+    def move_up(self):
+        x = self.head.point[0]
+        y = self.head.point[1] -1
+        new_square = Square((x,y), self.head.size)
+        self.add(new_square)
+
+    def move_down(self):
+        x = self.head.point[0]
+        y = self.head.point[1] +1
+        new_square = Square((x,y), self.head.size)
+        self.add(new_square)
+
+    def draw(self, canvas):
+        for square in self.snake_map.values():
+            square.draw(canvas)        
+
+class HomeBase:
+    
+    def __init__(self, start_pos, size):
+        self.start_pos = start_pos
+        self.home_base_map = {}
+        self.fill_home_base(size)
+    #(0,0)          (9,0)            (19,0)
+    
+    
+    
+   #            (4,4)            (14,4)
+    
+    
+   #            (4,14)           (14,14)
+    
+    
+    #(0,19)         (9,19)           (19,19)
+    def add(self, square):
+        point = square.point
+        pos_key = "%s-%s" % (point[0], point[1])
+        self.home_base_map[pos_key] = square
+        
+    def fill_home_base(self, size):
+        sp = self.start_pos
+        tl_x = (3 * sp[0])/4
+        tl_y = (3 * sp[1])/4
+        for x in range(size):
+            for y in range(size):
+                square = Square((x + tl_x, y + tl_y), SIZE)
+                self.add(square)
+
+    def draw(self, canvas):
+        for square in self.home_base_map.values():
+            square.draw(canvas)
+
+home_base = HomeBase((10,10), 5)
 home_square = Square((5, 5), SIZE)
 the_snake = Snake(home_square)
   
@@ -62,20 +121,20 @@ def move(key):
     if KEYMAP[LEFT] == key:
         the_snake.move_left()
     elif KEYMAP[RIGHT] == key:
-        print "right"
+        the_snake.move_right()
     elif KEYMAP[UP] == key:
-        print "up"
+        the_snake.move_up()
     elif KEYMAP[DOWN] == key:
-        print "down"
+        the_snake.move_down()
     else :
         print "unused"
 
 def draw(canvas):
-    for square in the_snake.snake_map.values():
-        square.draw(canvas)
-
+    the_snake.draw(canvas)
+    home_base.draw(canvas)
     
 frame = simplegui.create_frame("Home", *WINDOW)
 frame.set_draw_handler(draw)
 frame.set_keydown_handler(move)
 frame.start()
+
